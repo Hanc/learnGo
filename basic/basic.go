b6775c28df79f8a6e415e86a4a23979f05bb5f57
Hynp <1123598535@qq.com>
2021-02-26 23:53:37 +0800

    const and enums

--- basic/basic.go
+++ basic/basic.go
@@ -12,41 +12,69 @@ var (
 	bb = true
 )
 
-
-func variableZeroValue()  {
+func variableZeroValue() {
 	var a int
 	var s string
 	fmt.Printf("%d %q\n", a, s)
 }
 
-func variableInitialValue()  {
+func variableInitialValue() {
 	var a, b int = 3, 4
-	var s  string = "abc"
+	var s string = "abc"
 	fmt.Println(a, b, s)
 }
 
-func variableTypeDeduction()  {
-	var a, b, c , f = 3, 4, true, "def"
+func variableTypeDeduction() {
+	var a, b, c, f = 3, 4, true, "def"
 	fmt.Println(a, b, c, f)
 }
 
-func variableShorter()  {
+func variableShorter() {
 	a, b, c, s := 3, 4, true, "def1"
 	fmt.Println(a, b, c, s)
 }
 
-func euler()  {
-	fmt.Println(cmplx.Exp(1i * math.Pi) + 1)
+func euler() {
+	fmt.Println(cmplx.Exp(1i*math.Pi) + 1)
 }
 
-func triangle()  {
+func triangle() {
 	var a, b int = 3, 4
 	var c int
 	c = int(math.Sqrt(float64(a*a + b*b)))
 	fmt.Println(c)
 }
 
-func main()  {
+func consts() {
+	const filename = "abc.txt"
+	const a, b = 3, 4
+	var c int
+	c = int(math.Sqrt(a*a + b*b))
+	fmt.Println(c)
+}
+
+func enums() {
+	const (
+		cpp = iota
+		_
+		python
+		golang
+		javascript
+	)
+
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+	fmt.Println(cpp, javascript, python, golang)
+	fmt.Println(b, kb, mb, gb, tb, pb)
+}
+
+func main() {
 	fmt.Println("hello world")
 	variableZeroValue()
 	variableInitialValue()
@@ -55,4 +83,5 @@ func main()  {
 	fmt.Println(a1, bb, s2)
 	euler()
 	triangle()
+	enums()
 }
